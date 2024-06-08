2d4bd73e7c4af2d0b149cb465ec61a11c1d05991
luanpedrosa <83519092+luan048@users.noreply.github.com>
2024-06-08 08:13:24 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const Client = require('pg').Client
 
 const cliente = new Client({
-    user: "postgres",
-    password: "luan123",
+    user: "********",
+    password: "*******",
     host: "127.0.0.1",
     port: 5432,
     database: "Restaurant"
