52430c853326ef37edcfc3f292189f2e321ff821
luanpedrosa <83519092+luan048@users.noreply.github.com>
2024-06-09 14:30:44 -0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,27 @@
-// SERÁ PARA ADICIONAR AS FUNÇÕES EM RELAÇÃO AO RESERVATION
-//LIST RESTAURANT, CREATE RESERVATION ETC...
-//IMPORTAR DB, SERVICES e OUTROS CONTROLLERS
+const Cliente = require('../config/db')
+
+const {getRestaurant} = require('./restaurantController')
+
+// getRestaurant()
+
+const {insertReservation} = require('./reservationController')
+
+// insertReservation('Ally', 'Luan', 5, 8, '20:00:00')
+
+const {delReservation} = require('./reservationController')
+
+// delReservation()
+
+const {upReservation} = require('./reservationController')
+
+// upReservation()
+
+const { getUser, creatUser, userValidation, updateName, updateMail,  updatePassword,  delUser} = require('../services/userServices')
+
+// getUser('luki90@mail.com')
+// creatUser('Luan', 'luan@gmail.com', 1278)
+// userValidation('luki90@mail.com', 1278)
+// updateName('LuanP', 'luan10@gmail.com')
+// updateMail('LuanMont@hotmail.com', 'Lua1Mont@hotmail.com')
+// updatePassword('luan@gmail.com', '123')
+// delUser(8039)
